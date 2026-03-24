2202540583483c2952ac48460739afc9f6561fcf
dafra-fdshop <df.mailkonto@gmail.com>
2026-03-24 12:56:19 +0100

    Fix toolbar add action and normalize product table data handling

--- administrator/tmpl/categories/default.php
+++ administrator/tmpl/categories/default.php
@@ -7,43 +7,47 @@
 defined('_JEXEC') or die;
 ?>
 
-<div class="com-fdshop-categories">
-    <p>
-        <a class="btn btn-primary" href="index.php?option=com_fdshop&view=category&layout=edit">
-            Neu
-        </a>
-    </p>
+<form action="index.php?option=com_fdshop&view=categories" method="post" name="adminForm" id="adminForm">
+	<div class="com-fdshop-categories">
+		<p>
+			<a class="btn btn-primary" href="index.php?option=com_fdshop&view=category&layout=edit">
+				Neu
+			</a>
+		</p>
 
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Alias</th>
-                <th>Parent ID</th>
-                <th>Aktiv</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php if (!empty($this->items)) : ?>
-                <?php foreach ($this->items as $item) : ?>
-                    <tr>
-                        <td><?php echo (int) $item->id; ?></td>
-                        <td>
-                            <a href="index.php?option=com_fdshop&view=category&layout=edit&id=<?php echo (int) $item->id; ?>">
-                                <?php echo htmlspecialchars((string) $item->category_name, ENT_QUOTES, 'UTF-8'); ?>
-                            </a>
-                        </td>
-                        <td><?php echo htmlspecialchars((string) $item->alias, ENT_QUOTES, 'UTF-8'); ?></td>
-                        <td><?php echo (int) $item->parent_id; ?></td>
-                        <td><?php echo (int) $item->is_active; ?></td>
-                    </tr>
-                <?php endforeach; ?>
-            <?php else : ?>
-                <tr>
-                    <td colspan="5">Noch keine Kategorien vorhanden.</td>
-                </tr>
-            <?php endif; ?>
-        </tbody>
-    </table>
-</div>
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Name</th>
+					<th>Alias</th>
+					<th>Parent ID</th>
+					<th>Aktiv</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php if (!empty($this->items)) : ?>
+					<?php foreach ($this->items as $item) : ?>
+						<tr>
+							<td><?php echo (int) $item->id; ?></td>
+							<td>
+								<a href="index.php?option=com_fdshop&view=category&layout=edit&id=<?php echo (int) $item->id; ?>">
+									<?php echo htmlspecialchars((string) $item->category_name, ENT_QUOTES, 'UTF-8'); ?>
+								</a>
+							</td>
+							<td><?php echo htmlspecialchars((string) $item->alias, ENT_QUOTES, 'UTF-8'); ?></td>
+							<td><?php echo (int) $item->parent_id; ?></td>
+							<td><?php echo (int) $item->is_active; ?></td>
+						</tr>
+					<?php endforeach; ?>
+				<?php else : ?>
+					<tr>
+						<td colspan="5">Noch keine Kategorien vorhanden.</td>
+					</tr>
+				<?php endif; ?>
+			</tbody>
+		</table>
+	</div>
+	<input type="hidden" name="task" value="">
+		<?php echo \Joomla\CMS\HTML\HTMLHelper::_('form.token'); ?>
+</form>
