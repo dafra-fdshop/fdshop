07de74cfeb508787471ebeb4166c3c3788c0a16f
dafra-fdshop <df.mailkonto@gmail.com>
2026-03-27 12:20:37 +0100

    Update

--- administrator/src/Controller/CategoriesController.php
+++ administrator/src/Controller/CategoriesController.php
@@ -8,9 +8,14 @@ namespace FDShop\Component\FDShop\Administrator\Controller;
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\MVC\Controller\BaseController;
+use Joomla\CMS\MVC\Controller\AdminController;
 
-class CategoriesController extends BaseController
+class CategoriesController extends AdminController
 {
     protected $default_view = 'categories';
+
+    public function getModel($name = 'Category', $prefix = '', $config = ['ignore_request' => true])
+    {
+        return parent::getModel($name, $prefix, $config);
+    }
 }
--- administrator/tmpl/categories/default.php
+++ administrator/tmpl/categories/default.php
@@ -5,49 +5,59 @@
  */
 
 defined('_JEXEC') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
+
+HTMLHelper::_('behavior.multiselect');
+HTMLHelper::_('draggablelist.draggable');
 ?>
 
 <form action="index.php?option=com_fdshop&view=categories" method="post" name="adminForm" id="adminForm">
-	<div class="com-fdshop-categories">
-		<p>
-			<a class="btn btn-primary" href="index.php?option=com_fdshop&view=category&layout=edit">
-				Neu
-			</a>
-		</p>
 
-		<table class="table table-striped">
-			<thead>
-				<tr>
-					<th>ID</th>
-					<th>Name</th>
-					<th>Alias</th>
-					<th>Parent ID</th>
-					<th>Aktiv</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php if (!empty($this->items)) : ?>
-					<?php foreach ($this->items as $item) : ?>
-						<tr>
-							<td><?php echo (int) $item->id; ?></td>
-							<td>
-								<a href="index.php?option=com_fdshop&view=category&layout=edit&id=<?php echo (int) $item->id; ?>">
-									<?php echo htmlspecialchars((string) $item->category_name, ENT_QUOTES, 'UTF-8'); ?>
-								</a>
-							</td>
-							<td><?php echo htmlspecialchars((string) $item->alias, ENT_QUOTES, 'UTF-8'); ?></td>
-							<td><?php echo (int) $item->parent_id; ?></td>
-							<td><?php echo (int) $item->is_active; ?></td>
-						</tr>
-					<?php endforeach; ?>
-				<?php else : ?>
-					<tr>
-						<td colspan="5">Noch keine Kategorien vorhanden.</td>
-					</tr>
-				<?php endif; ?>
-			</tbody>
-		</table>
-	</div>
-	<input type="hidden" name="task" value="">
-		<?php echo \Joomla\CMS\HTML\HTMLHelper::_('form.token'); ?>
+<table class="table table-striped" id="categoryList">
+    <thead>
+        <tr>
+            <th width="1%">#</th>
+            <th width="1%"><?php echo HTMLHelper::_('grid.checkall'); ?></th>
+            <th width="5%">Order</th>
+            <th>Name</th>
+            <th>Alias</th>
+            <th>Aktiv</th>
+            <th>ID</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <?php foreach ($this->items as $i => $item) : ?>
+            <tr>
+                <td><?php echo $i + 1; ?></td>
+
+                <td>
+                    <?php echo HTMLHelper::_('grid.id', $i, $item->id); ?>
+                </td>
+
+                <td class="order nowrap">
+                    <?php echo HTMLHelper::_('grid.order', $this->items, 'filesave.png', 'categories.saveorder'); ?>
+                </td>
+
+                <td>
+                    <a href="index.php?option=com_fdshop&view=category&layout=edit&id=<?php echo $item->id; ?>">
+                        <?php echo $this->escape($item->category_name); ?>
+                    </a>
+                </td>
+
+                <td><?php echo $this->escape($item->alias); ?></td>
+
+                <td>
+                    <?php echo HTMLHelper::_('grid.published', $item->is_active, $i, 'categories.', true); ?>
+                </td>
+
+                <td><?php echo $item->id; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+<input type="hidden" name="task" value="">
+<?php echo HTMLHelper::_('form.token'); ?>
 </form>
