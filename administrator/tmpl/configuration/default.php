eddbdb709ad634c05caf0a134d1f80935e7a38ef
dafra-fdshop <df.mailkonto@gmail.com>
2026-03-29 09:19:59 +0200

    Refactor templates to admin list v2

--- administrator/tmpl/configuration/default.php
+++ administrator/tmpl/configuration/default.php
@@ -7,139 +7,284 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Layout\LayoutHelper;
+use Joomla\CMS\Router\Route;
+
+HTMLHelper::_('bootstrap.tooltip');
+
+$user = $this->getCurrentUser();
+
+$shipmentListOrder = $this->shipmentState->get('list.ordering');
+$shipmentListDirn  = $this->shipmentState->get('list.direction');
+
+$paymentListOrder = $this->paymentState->get('list.ordering');
+$paymentListDirn  = $this->paymentState->get('list.direction');
+
+$shipmentSearchView = (object) [
+	'filterForm'     => $this->shipmentFilterForm,
+	'activeFilters'  => $this->shipmentActiveFilters,
+];
+
+$paymentSearchView = (object) [
+	'filterForm'     => $this->paymentFilterForm,
+	'activeFilters'  => $this->paymentActiveFilters,
+];
 ?>
 
-<form action="index.php?option=com_fdshop&view=configuration" method="post" name="adminForm" id="adminForm">
-    <?php echo HTMLHelper::_('uitab.startTabSet', 'fdshopConfigurationTabs', ['active' => 'general']); ?>
+<form action="<?php echo Route::_('index.php?option=com_fdshop&view=configuration'); ?>" method="post" name="adminForm" id="adminForm">
+	<?php echo HTMLHelper::_('uitab.startTabSet', 'fdshopConfigurationTabs', ['active' => 'general']); ?>
 
-    <?php echo HTMLHelper::_('uitab.addTab', 'fdshopConfigurationTabs', 'general', 'Allgemein'); ?>
-        <div class="row">
-            <div class="col-12 col-xl-8">
-                <div class="card mb-3">
-                    <div class="card-header">Allgemein</div>
-                    <div class="card-body">
-                        <?php echo $this->form->renderField('general_vat_rate'); ?>
-                        <?php echo $this->form->renderField('show_terms_checkbox'); ?>
-                        <?php echo $this->form->renderField('require_terms_checkbox'); ?>
-                    </div>
-                </div>
-            </div>
-        </div>
-    <?php echo HTMLHelper::_('uitab.endTab'); ?>
+	<?php echo HTMLHelper::_('uitab.addTab', 'fdshopConfigurationTabs', 'general', 'Allgemein'); ?>
+		<div class="row">
+			<div class="col-12 col-xl-8">
+				<div class="card mb-3">
+					<div class="card-header">Allgemein</div>
+					<div class="card-body">
+						<?php echo $this->form->renderField('general_vat_rate'); ?>
+						<?php echo $this->form->renderField('show_terms_checkbox'); ?>
+						<?php echo $this->form->renderField('require_terms_checkbox'); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php echo HTMLHelper::_('uitab.endTab'); ?>
 
-    <?php echo HTMLHelper::_('uitab.addTab', 'fdshopConfigurationTabs', 'images', 'Bilder'); ?>
-        <div class="row">
-            <div class="col-12 col-xl-8">
-                <div class="card mb-3">
-                    <div class="card-header">Bilder</div>
-                    <div class="card-body">
-                        <?php echo $this->form->renderField('image_size_default'); ?>
-                        <?php echo $this->form->renderField('image_size_small'); ?>
-                        <?php echo $this->form->renderField('image_size_mobile'); ?>
-                        <?php echo $this->form->renderField('image_size_manufacturer'); ?>
-                    </div>
-                </div>
-            </div>
-        </div>
-    <?php echo HTMLHelper::_('uitab.endTab'); ?>
+	<?php echo HTMLHelper::_('uitab.addTab', 'fdshopConfigurationTabs', 'images', 'Bilder'); ?>
+		<div class="row">
+			<div class="col-12 col-xl-8">
+				<div class="card mb-3">
+					<div class="card-header">Bilder</div>
+					<div class="card-body">
+						<?php echo $this->form->renderField('image_size_default'); ?>
+						<?php echo $this->form->renderField('image_size_small'); ?>
+						<?php echo $this->form->renderField('image_size_mobile'); ?>
+						<?php echo $this->form->renderField('image_size_manufacturer'); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php echo HTMLHelper::_('uitab.endTab'); ?>
 
-    <?php echo HTMLHelper::_('uitab.addTab', 'fdshopConfigurationTabs', 'shipments', 'Versand'); ?>
-        <div class="card mb-3">
-            <div class="card-header">Versandarten</div>
-            <div class="card-body">
-                <p>
-                    <a class="btn btn-primary" href="index.php?option=com_fdshop&view=shipment&layout=edit">
-                        Hinzufügen
-                    </a>
-                </p>
+	<?php echo HTMLHelper::_('uitab.addTab', 'fdshopConfigurationTabs', 'shipments', 'Versand'); ?>
+		<div class="card mb-3">
+			<div class="card-header">Versandarten</div>
+			<div class="card-body">
+				<p>
+					<a class="btn btn-primary" href="<?php echo Route::_('index.php?option=com_fdshop&view=shipment&layout=edit'); ?>">
+						Hinzufügen
+					</a>
+				</p>
 
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Versandname</th>
-                            <th>Farbe</th>
-                            <th>Preis</th>
-                            <th>Veröffentlicht</th>
-                            <th>ID</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php if (!empty($this->shipments)) : ?>
-                            <?php foreach ($this->shipments as $item) : ?>
-                                <tr>
-                                    <td>
-                                        <a href="index.php?option=com_fdshop&view=shipment&layout=edit&id=<?php echo (int) $item->id; ?>">
-                                            <?php echo htmlspecialchars((string) $item->shipment_name, ENT_QUOTES, 'UTF-8'); ?>
-                                        </a>
-                                    </td>
-                                    <td><?php echo htmlspecialchars((string) $item->shipment_color, ENT_QUOTES, 'UTF-8'); ?></td>
-                                    <td><?php echo htmlspecialchars((string) $item->shipment_price, ENT_QUOTES, 'UTF-8'); ?></td>
-                                    <td><?php echo (int) $item->is_published; ?></td>
-                                    <td><?php echo (int) $item->id; ?></td>
-                                </tr>
-                            <?php endforeach; ?>
-                        <?php else : ?>
-                            <tr>
-                                <td colspan="5">Noch keine Versandarten vorhanden.</td>
-                            </tr>
-                        <?php endif; ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    <?php echo HTMLHelper::_('uitab.endTab'); ?>
+				<?php echo LayoutHelper::render('joomla.searchtools.default', ['view' => $shipmentSearchView]); ?>
 
-    <?php echo HTMLHelper::_('uitab.addTab', 'fdshopConfigurationTabs', 'paymentmethods', 'Bezahlsystem'); ?>
-        <div class="card mb-3">
-            <div class="card-header">Zahlungsarten</div>
-            <div class="card-body">
-                <p>
-                    <a class="btn btn-primary" href="index.php?option=com_fdshop&view=paymentmethod&layout=edit">
-                        Hinzufügen
-                    </a>
-                </p>
+				<div class="table-responsive">
+					<table class="table itemList" id="shipmentList">
+						<thead>
+							<tr>
+								<td class="w-1 text-center">
+									<?php echo HTMLHelper::_('grid.checkall'); ?>
+								</td>
+								<th scope="col">
+									<?php echo HTMLHelper::_('searchtools.sort', 'Versandname', 'a.shipment_name', $shipmentListDirn, $shipmentListOrder); ?>
+								</th>
+								<th scope="col">
+									<?php echo HTMLHelper::_('searchtools.sort', 'Beschreibung', 'a.shipment_description', $shipmentListDirn, $shipmentListOrder); ?>
+								</th>
+								<th scope="col" class="text-center">
+									<?php echo HTMLHelper::_('searchtools.sort', 'Farbe', 'a.shipment_color', $shipmentListDirn, $shipmentListOrder); ?>
+								</th>
+								<th scope="col">
+									<?php echo HTMLHelper::_('searchtools.sort', 'Gebühr', 'a.shipment_price', $shipmentListDirn, $shipmentListOrder); ?>
+								</th>
+								<th scope="col" class="w-1 text-center">
+									<?php echo HTMLHelper::_('searchtools.sort', 'Veröffentlicht', 'a.is_published', $shipmentListDirn, $shipmentListOrder); ?>
+								</th>
+								<th scope="col" class="w-1 text-center">
+									<?php echo HTMLHelper::_('searchtools.sort', 'ID', 'a.id', $shipmentListDirn, $shipmentListOrder); ?>
+								</th>
+							</tr>
+						</thead>
 
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Zahlungsname</th>
-                            <th>Gebühr</th>
-                            <th>PayPal</th>
-                            <th>Veröffentlicht</th>
-                            <th>ID</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php if (!empty($this->paymentmethods)) : ?>
-                            <?php foreach ($this->paymentmethods as $item) : ?>
-                                <tr>
-                                    <td>
-                                        <a href="index.php?option=com_fdshop&view=paymentmethod&layout=edit&id=<?php echo (int) $item->id; ?>">
-                                            <?php echo htmlspecialchars((string) $item->payment_name, ENT_QUOTES, 'UTF-8'); ?>
-                                        </a>
-                                    </td>
-                                    <td><?php echo htmlspecialchars((string) $item->payment_fee, ENT_QUOTES, 'UTF-8'); ?></td>
-                                    <td><?php echo (int) $item->paypal_enabled; ?></td>
-                                    <td><?php echo (int) $item->is_published; ?></td>
-                                    <td><?php echo (int) $item->id; ?></td>
-                                <?php endforeach; ?>
-                            </tr>
-                        <?php else : ?>
-                            <tr>
-                                <td colspan="5">Noch keine Zahlungsarten vorhanden.</td>
-                            </tr>
-                        <?php endif; ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    <?php echo HTMLHelper::_('uitab.endTab'); ?>
+						<tbody>
+							<?php if (!empty($this->shipments)) : ?>
+								<?php foreach ($this->shipments as $i => $item) : ?>
+									<?php
+									$editLink  = Route::_('index.php?option=com_fdshop&view=shipment&layout=edit&id=' . (int) $item->id);
+									$canEdit   = $user->authorise('core.edit', 'com_fdshop');
+									$canChange = $user->authorise('core.edit.state', 'com_fdshop');
+									$color     = trim((string) ($item->shipment_color ?? ''));
+									?>
+									<tr class="row<?php echo $i % 2; ?>">
+										<td class="text-center">
+											<?php echo HTMLHelper::_('grid.id', $i, (int) $item->id); ?>
+										</td>
 
-    <?php echo HTMLHelper::_('uitab.endTabSet'); ?>
+										<th scope="row">
+											<?php if ($canEdit) : ?>
+												<a href="<?php echo $editLink; ?>">
+													<?php echo $this->escape((string) $item->shipment_name); ?>
+												</a>
+											<?php else : ?>
+												<?php echo $this->escape((string) $item->shipment_name); ?>
+											<?php endif; ?>
+										</th>
 
-    <?php echo $this->form->renderField('id'); ?>
-    <input type="hidden" name="task" value="">
-    <?php echo HTMLHelper::_('form.token'); ?>
+										<td>
+											<?php echo $item->shipment_description ?? ''; ?>
+										</td>
+
+										<td class="text-center">
+											<?php if ($color !== '') : ?>
+												<span style="display:inline-block;width:16px;height:16px;background:<?php echo htmlspecialchars($color, ENT_QUOTES, 'UTF-8'); ?>;"></span>
+											<?php endif; ?>
+										</td>
+
+										<td>
+											<?php echo $this->escape((string) $item->shipment_price); ?>
+										</td>
+
+										<td class="text-center">
+											<?php echo HTMLHelper::_('jgrid.published', (int) $item->published, $i, 'shipments.', $canChange, 'cb'); ?>
+										</td>
+
+										<td class="text-center">
+											<?php echo (int) $item->id; ?>
+										</td>
+									</tr>
+								<?php endforeach; ?>
+							<?php else : ?>
+								<tr>
+									<td colspan="7" class="text-center">
+										Keine Versandarten vorhanden.
+									</td>
+								</tr>
+							<?php endif; ?>
+						</tbody>
+
+						<?php if (!empty($this->shipments)) : ?>
+							<tfoot>
+								<tr>
+									<td colspan="7">
+										<?php echo $this->shipmentPagination->getListFooter(); ?>
+									</td>
+								</tr>
+							</tfoot>
+						<?php endif; ?>
+					</table>
+				</div>
+
+				<?php echo $this->shipmentFilterForm->renderControlFields(); ?>
+			</div>
+		</div>
+	<?php echo HTMLHelper::_('uitab.endTab'); ?>
+
+	<?php echo HTMLHelper::_('uitab.addTab', 'fdshopConfigurationTabs', 'paymentmethods', 'Bezahlsystem'); ?>
+		<div class="card mb-3">
+			<div class="card-header">Zahlungsarten</div>
+			<div class="card-body">
+				<p>
+					<a class="btn btn-primary" href="<?php echo Route::_('index.php?option=com_fdshop&view=paymentmethod&layout=edit'); ?>">
+						Hinzufügen
+					</a>
+				</p>
+
+				<?php echo LayoutHelper::render('joomla.searchtools.default', ['view' => $paymentSearchView]); ?>
+
+				<div class="table-responsive">
+					<table class="table itemList" id="paymentmethodList">
+						<thead>
+							<tr>
+								<td class="w-1 text-center">
+									<?php echo HTMLHelper::_('grid.checkall'); ?>
+								</td>
+								<th scope="col">
+									<?php echo HTMLHelper::_('searchtools.sort', 'Zahlungsname', 'a.payment_name', $paymentListDirn, $paymentListOrder); ?>
+								</th>
+								<th scope="col">
+									<?php echo HTMLHelper::_('searchtools.sort', 'Beschreibung', 'a.payment_description', $paymentListDirn, $paymentListOrder); ?>
+								</th>
+								<th scope="col">
+									<?php echo HTMLHelper::_('searchtools.sort', 'Gebühr', 'a.payment_fee', $paymentListDirn, $paymentListOrder); ?>
+								</th>
+								<th scope="col" class="w-1 text-center">
+									<?php echo HTMLHelper::_('searchtools.sort', 'Veröffentlicht', 'a.is_published', $paymentListDirn, $paymentListOrder); ?>
+								</th>
+								<th scope="col" class="w-1 text-center">
+									<?php echo HTMLHelper::_('searchtools.sort', 'ID', 'a.id', $paymentListDirn, $paymentListOrder); ?>
+								</th>
+							</tr>
+						</thead>
+
+						<tbody>
+							<?php if (!empty($this->paymentmethods)) : ?>
+								<?php foreach ($this->paymentmethods as $i => $item) : ?>
+									<?php
+									$editLink  = Route::_('index.php?option=com_fdshop&view=paymentmethod&layout=edit&id=' . (int) $item->id);
+									$canEdit   = $user->authorise('core.edit', 'com_fdshop');
+									$canChange = $user->authorise('core.edit.state', 'com_fdshop');
+									?>
+									<tr class="row<?php echo $i % 2; ?>">
+										<td class="text-center">
+											<?php echo HTMLHelper::_('grid.id', $i, (int) $item->id); ?>
+										</td>
+
+										<th scope="row">
+											<?php if ($canEdit) : ?>
+												<a href="<?php echo $editLink; ?>">
+													<?php echo $this->escape((string) $item->payment_name); ?>
+												</a>
+											<?php else : ?>
+												<?php echo $this->escape((string) $item->payment_name); ?>
+											<?php endif; ?>
+										</th>
+
+										<td>
+											<?php echo $item->payment_description ?? ''; ?>
+										</td>
+
+										<td>
+											<?php echo $this->escape((string) $item->payment_fee); ?>
+										</td>
+
+										<td class="text-center">
+											<?php echo HTMLHelper::_('jgrid.published', (int) $item->published, $i, 'paymentmethods.', $canChange, 'cb'); ?>
+										</td>
+
+										<td class="text-center">
+											<?php echo (int) $item->id; ?>
+										</td>
+									</tr>
+								<?php endforeach; ?>
+							<?php else : ?>
+								<tr>
+									<td colspan="6" class="text-center">
+										Keine Zahlungsarten vorhanden.
+									</td>
+								</tr>
+							<?php endif; ?>
+						</tbody>
+
+						<?php if (!empty($this->paymentmethods)) : ?>
+							<tfoot>
+								<tr>
+									<td colspan="6">
+										<?php echo $this->paymentPagination->getListFooter(); ?>
+									</td>
+								</tr>
+							</tfoot>
+						<?php endif; ?>
+					</table>
+				</div>
+
+				<?php echo $this->paymentFilterForm->renderControlFields(); ?>
+			</div>
+		</div>
+	<?php echo HTMLHelper::_('uitab.endTab'); ?>
+
+	<?php echo HTMLHelper::_('uitab.endTabSet'); ?>
+
+	<?php echo $this->form->renderField('id'); ?>
+	<?php echo HTMLHelper::_('form.token'); ?>
 </form>
 
 <script>
--- administrator/tmpl/manufacturers/default.php
+++ administrator/tmpl/manufacturers/default.php
@@ -5,45 +5,105 @@
  */
 
 defined('_JEXEC') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Layout\LayoutHelper;
+use Joomla\CMS\Router\Route;
+
+HTMLHelper::_('bootstrap.tooltip');
+
+$user      = $this->getCurrentUser();
+$listOrder = $this->state->get('list.ordering');
+$listDirn  = $this->state->get('list.direction');
 ?>
 
-<form action="index.php?option=com_fdshop&view=manufacturers" method="post" name="adminForm" id="adminForm">
-	<div class="com-fdshop-manufacturers">	
-		<p>
-			<a class="btn btn-primary" href="index.php?option=com_fdshop&view=manufacturer&layout=edit">
-				Neu
-			</a>
-		</p>
+<form action="<?php echo Route::_('index.php?option=com_fdshop&view=manufacturers'); ?>" method="post" name="adminForm" id="adminForm">
+	<?php echo LayoutHelper::render('joomla.searchtools.default', ['view' => $this]); ?>
 
-		<table class="table table-striped">
+	<div class="table-responsive">
+		<table class="table itemList" id="manufacturerList">
 			<thead>
 				<tr>
-					<th><?php echo htmlspecialchars('Herstellername', ENT_QUOTES, 'UTF-8'); ?></th>
-					<th><?php echo htmlspecialchars('Veröffentlicht', ENT_QUOTES, 'UTF-8'); ?></th>
-					<th><?php echo htmlspecialchars('ID', ENT_QUOTES, 'UTF-8'); ?></th>
+					<td class="w-1 text-center">
+						<?php echo HTMLHelper::_('grid.checkall'); ?>
+					</td>
+					<th scope="col">
+						<?php echo HTMLHelper::_('searchtools.sort', 'Name', 'a.manufacturer_name', $listDirn, $listOrder); ?>
+					</th>
+					<th scope="col">
+						<?php echo HTMLHelper::_('searchtools.sort', 'Beschreibung', 'a.description', $listDirn, $listOrder); ?>
+					</th>
+					<th scope="col" class="w-1 text-center">
+						<?php echo HTMLHelper::_('searchtools.sort', 'Veröffentlicht', 'a.is_active', $listDirn, $listOrder); ?>
+					</th>
+					<th scope="col" class="w-1 text-center">
+						<?php echo HTMLHelper::_('searchtools.sort', 'ID', 'a.id', $listDirn, $listOrder); ?>
+					</th>
 				</tr>
 			</thead>
+
 			<tbody>
 				<?php if (!empty($this->items)) : ?>
-					<?php foreach ($this->items as $item) : ?>
-						<tr>
-							<td>
-								<a href="index.php?option=com_fdshop&view=manufacturer&layout=edit&id=<?php echo (int) $item->id; ?>">
-									<?php echo htmlspecialchars((string) $item->manufacturer_name, ENT_QUOTES, 'UTF-8'); ?>
-								</a>
+					<?php foreach ($this->items as $i => $item) : ?>
+						<?php
+						$editLink  = Route::_('index.php?option=com_fdshop&view=manufacturer&layout=edit&id=' . (int) $item->id);
+						$canEdit   = $user->authorise('core.edit', 'com_fdshop');
+						$canChange = $user->authorise('core.edit.state', 'com_fdshop');
+						?>
+						<tr class="row<?php echo $i % 2; ?>">
+							<td class="text-center">
+								<?php echo HTMLHelper::_('grid.id', $i, (int) $item->id); ?>
 							</td>
-							<td><?php echo (int) $item->is_active; ?></td>
-							<td><?php echo (int) $item->id; ?></td>
+
+							<th scope="row">
+								<?php if ($canEdit) : ?>
+									<a href="<?php echo $editLink; ?>">
+										<?php echo $this->escape((string) $item->manufacturer_name); ?>
+									</a>
+								<?php else : ?>
+									<?php echo $this->escape((string) $item->manufacturer_name); ?>
+								<?php endif; ?>
+
+								<?php if (!empty($item->alias)) : ?>
+									<div class="small text-muted">
+										<?php echo $this->escape((string) $item->alias); ?>
+									</div>
+								<?php endif; ?>
+							</th>
+
+							<td>
+								<?php echo $item->description ?? ''; ?>
+							</td>
+
+							<td class="text-center">
+								<?php echo HTMLHelper::_('jgrid.published', (int) $item->published, $i, 'manufacturers.', $canChange, 'cb'); ?>
+							</td>
+
+							<td class="text-center">
+								<?php echo (int) $item->id; ?>
+							</td>
 						</tr>
 					<?php endforeach; ?>
 				<?php else : ?>
 					<tr>
-						<td colspan="3">Noch keine Hersteller vorhanden.</td>
+						<td colspan="5" class="text-center">
+							Keine Hersteller vorhanden.
+						</td>
 					</tr>
 				<?php endif; ?>
 			</tbody>
+
+			<?php if (!empty($this->items)) : ?>
+				<tfoot>
+					<tr>
+						<td colspan="5">
+							<?php echo $this->pagination->getListFooter(); ?>
+						</td>
+					</tr>
+				</tfoot>
+			<?php endif; ?>
 		</table>
 	</div>
-	<input type="hidden" name="task" value="">
-		<?php echo \Joomla\CMS\HTML\HTMLHelper::_('form.token'); ?>
+
+	<?php echo $this->filterForm->renderControlFields(); ?>
 </form>
