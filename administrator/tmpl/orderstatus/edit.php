35584ed14781c143ee1d931588526e4e600cbd95
dafra-fdshop <df.mailkonto@gmail.com>
2026-04-01 17:25:10 +0200

    Fix

--- administrator/tmpl/orderstatus/edit.php
+++ administrator/tmpl/orderstatus/edit.php
@@ -8,6 +8,12 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Router\Route;
+
+$buyerEmailMode = match ((string) ($this->item->buyer_email_mode ?? '')) {
+    'account' => 'Ja',
+    'none'    => 'Nein',
+    default   => (string) ($this->item->buyer_email_mode ?? ''),
+};
 ?>
 
 <form action="<?php echo Route::_('index.php?option=com_fdshop&view=orderstatus&layout=edit&id=' . (int) ($this->item->id ?? 0)); ?>" method="post" name="adminForm" id="adminForm">
@@ -17,44 +23,26 @@ use Joomla\CMS\Router\Route;
 				<div class="card-header">Bestellstatus</div>
 				<div class="card-body">
 					<div class="mb-3">
-						<div class="fw-bold">Bezeichnung</div>
-						<div><?php echo $this->escape((string) ($this->item->status_name ?? '')); ?></div>
-					</div>
-
-					<div class="mb-3">
 						<div class="fw-bold">Status-Code</div>
 						<div><?php echo $this->escape((string) ($this->item->status_code ?? '')); ?></div>
 					</div>
 
 					<div class="mb-3">
-						<div class="fw-bold">E-Mail Verkäufer</div>
-						<div><?php echo $this->escape((string) ($this->item->seller_email_mode_label ?? '')); ?></div>
-						<?php if (($this->item->seller_email_mode ?? '') === 'custom' && !empty($this->item->seller_email_address)) : ?>
-							<div class="small text-muted">
-								<?php echo $this->escape((string) $this->item->seller_email_address); ?>
-							</div>
-						<?php endif; ?>
-					</div>
-
-					<div class="mb-3">
 						<div class="fw-bold">E-Mail Käufer</div>
-						<div><?php echo $this->escape((string) ($this->item->buyer_email_mode_label ?? '')); ?></div>
+						<div><?php echo $this->escape($buyerEmailMode); ?></div>
 					</div>
 
-					<div class="mb-3">
-						<div class="fw-bold">Rechnung</div>
-						<div><?php echo $this->escape((string) ($this->item->create_invoice_label ?? '')); ?></div>
-					</div>
-
-					<div class="mb-0">
-						<div class="fw-bold">Bestand</div>
-						<div><?php echo $this->escape((string) ($this->item->stock_action_label ?? '')); ?></div>
-					</div>
+					<?php echo $this->form->renderField('status_name'); ?>
+					<?php echo $this->form->renderField('seller_email_mode'); ?>
+					<?php echo $this->form->renderField('seller_email_address'); ?>
+					<?php echo $this->form->renderField('create_invoice'); ?>
+					<?php echo $this->form->renderField('stock_action'); ?>
 				</div>
 			</div>
 		</div>
 	</div>
 
-	<input type="hidden" name="id" value="<?php echo (int) ($this->item->id ?? 0); ?>">
+	<?php echo $this->form->renderField('id'); ?>
+	<input type="hidden" name="task" value="">
 	<?php echo HTMLHelper::_('form.token'); ?>
 </form>
