2f559d1878fe87a351e41eebd08086db3feee98d
dafra-fdshop <df.mailkonto@gmail.com>
2026-04-08 14:28:00 +0200

    Extend order list template with payment shipment email and paid columns

--- administrator/tmpl/orders/default.php
+++ administrator/tmpl/orders/default.php
@@ -12,9 +12,10 @@ use Joomla\CMS\Router\Route;
 
 HTMLHelper::_('bootstrap.tooltip');
 
-$user      = $this->getCurrentUser();
-$listOrder = $this->state->get('list.ordering');
-$listDirn  = $this->state->get('list.direction');
+$user          = $this->getCurrentUser();
+$listOrder     = $this->state->get('list.ordering');
+$listDirn      = $this->state->get('list.direction');
+$statusOptions = $this->statusOptions ?? [];
 ?>
 
 <form action="<?php echo Route::_('index.php?option=com_fdshop&view=orders'); ?>" method="post" name="adminForm" id="adminForm">
@@ -28,22 +29,31 @@ $listDirn  = $this->state->get('list.direction');
 						<?php echo HTMLHelper::_('grid.checkall'); ?>
 					</td>
 					<th scope="col">
-						<?php echo HTMLHelper::_('searchtools.sort', 'Bestellnummer', 'a.order_number', $listDirn, $listOrder); ?>
+						<?php echo HTMLHelper::_('searchtools.sort', 'Bestellnummer/Rechnungsnummer', 'a.order_number', $listDirn, $listOrder); ?>
 					</th>
 					<th scope="col">
-						<?php echo HTMLHelper::_('searchtools.sort', 'Kunde', 'u.name', $listDirn, $listOrder); ?>
+						<?php echo HTMLHelper::_('searchtools.sort', 'Bestellstatus', 'os.status_name', $listDirn, $listOrder); ?>
 					</th>
 					<th scope="col">
-						<?php echo HTMLHelper::_('searchtools.sort', 'Status', 'os.status_name', $listDirn, $listOrder); ?>
+						<?php echo HTMLHelper::_('searchtools.sort', 'Kunde/Email', 'u.name', $listDirn, $listOrder); ?>
 					</th>
 					<th scope="col">
-						<?php echo HTMLHelper::_('searchtools.sort', 'Gesamtbetrag', 'a.grand_total', $listDirn, $listOrder); ?>
+						<?php echo HTMLHelper::_('searchtools.sort', 'Zahlungsart', 'pm.payment_name', $listDirn, $listOrder); ?>
 					</th>
 					<th scope="col">
-						<?php echo HTMLHelper::_('searchtools.sort', 'Datum', 'a.created', $listDirn, $listOrder); ?>
+						<?php echo HTMLHelper::_('searchtools.sort', 'Versandart', 's.shipment_name', $listDirn, $listOrder); ?>
 					</th>
-					<th scope="col" class="w-1 text-center">
-						<?php echo HTMLHelper::_('searchtools.sort', 'ID', 'a.id', $listDirn, $listOrder); ?>
+					<th scope="col">
+						<?php echo HTMLHelper::_('searchtools.sort', 'Datum (bestellt)', 'a.created', $listDirn, $listOrder); ?>
+					</th>
+					<th scope="col">
+						<?php echo HTMLHelper::_('searchtools.sort', 'Datum (geändert)', 'a.modified', $listDirn, $listOrder); ?>
+					</th>
+					<th scope="col" class="text-center">
+						Bezahlt
+					</th>
+					<th scope="col">
+						<?php echo HTMLHelper::_('searchtools.sort', 'Betrag', 'a.grand_total', $listDirn, $listOrder); ?>
 					</th>
 				</tr>
 			</thead>
@@ -52,9 +62,11 @@ $listDirn  = $this->state->get('list.direction');
 				<?php if (!empty($this->items)) : ?>
 					<?php foreach ($this->items as $i => $item) : ?>
 						<?php
-						$orderLink    = Route::_('index.php?option=com_fdshop&view=order&id=' . (int) $item->id);
-						$customerLink = '';
-						$amount       = number_format((float) ($item->grand_total ?? 0), 2, ',', '.');
+						$orderLink       = Route::_('index.php?option=com_fdshop&view=order&id=' . (int) $item->id);
+						$customerLink    = '';
+						$amount          = number_format((float) ($item->grand_total ?? 0), 2, ',', '.');
+						$shipmentStyle   = '';
+						$currentStatusId = (int) ($item->order_status_id ?? 0);
 
 						if (!empty($item->currency)) {
 							$amount .= ' ' . $this->escape((string) $item->currency);
@@ -62,6 +74,10 @@ $listDirn  = $this->state->get('list.direction');
 
 						if (!empty($item->user_id)) {
 							$customerLink = Route::_('index.php?option=com_users&task=user.edit&id=' . (int) $item->user_id);
+						}
+
+						if (!empty($item->shipment_color)) {
+							$shipmentStyle = 'background-color: ' . $this->escape((string) $item->shipment_color) . ';';
 						}
 						?>
 						<tr class="row<?php echo $i % 2; ?>">
@@ -71,40 +87,73 @@ $listDirn  = $this->state->get('list.direction');
 
 							<th scope="row">
 								<a href="<?php echo $orderLink; ?>">
-									<?php echo $this->escape((string) $item->order_number); ?>
+									<?php echo $this->escape((string) ($item->order_number ?? '')); ?>
 								</a>
+								<div class="small text-muted">—</div>
 							</th>
 
 							<td>
-								<?php if ($customerLink !== '') : ?>
-									<a href="<?php echo $customerLink; ?>">
-										<?php echo $this->escape((string) ($item->customer_name ?? '')); ?>
-									</a>
-								<?php else : ?>
-									<?php echo $this->escape((string) ($item->customer_name ?? '')); ?>
-								<?php endif; ?>
+								<select class="form-select form-select-sm" disabled>
+									<?php foreach ($statusOptions as $option) : ?>
+										<?php
+										$optionValue = (int) ($option->value ?? 0);
+
+										if ($optionValue <= 0) {
+											continue;
+										}
+										?>
+										<option value="<?php echo $optionValue; ?>" <?php echo $optionValue === $currentStatusId ? 'selected' : ''; ?>>
+											<?php echo $this->escape((string) ($option->text ?? '')); ?>
+										</option>
+									<?php endforeach; ?>
+								</select>
 							</td>
 
 							<td>
-								<?php echo $this->escape((string) ($item->status_name ?? '')); ?>
+								<div>
+									<?php if ($customerLink !== '') : ?>
+										<a href="<?php echo $customerLink; ?>">
+											<?php echo $this->escape((string) ($item->customer_name ?? '')); ?>
+										</a>
+									<?php else : ?>
+										<?php echo $this->escape((string) ($item->customer_name ?? '')); ?>
+									<?php endif; ?>
+								</div>
+								<div class="small text-muted">
+									<?php echo $this->escape((string) ($item->customer_email ?? '')); ?>
+								</div>
+							</td>
+
+							<td>
+								<?php echo $this->escape((string) ($item->payment_name ?? '')); ?>
+							</td>
+
+							<td>
+								<span class="d-inline-block px-2 py-1 rounded" style="<?php echo $shipmentStyle; ?>">
+									<?php echo $this->escape((string) ($item->shipment_name ?? '')); ?>
+								</span>
+							</td>
+
+							<td>
+								<?php echo !empty($item->created) ? HTMLHelper::_('date', $item->created, 'Y-m-d H:i') : ''; ?>
+							</td>
+
+							<td>
+								<?php echo !empty($item->modified) ? HTMLHelper::_('date', $item->modified, 'Y-m-d H:i') : ''; ?>
+							</td>
+
+							<td class="text-center">
+								<?php echo !empty($item->is_paid) ? '✔' : '✘'; ?>
 							</td>
 
 							<td>
 								<?php echo $amount; ?>
 							</td>
-
-							<td>
-								<?php echo HTMLHelper::_('date', $item->created, 'Y-m-d H:i'); ?>
-							</td>
-
-							<td class="text-center">
-								<?php echo (int) $item->id; ?>
-							</td>
 						</tr>
 					<?php endforeach; ?>
 				<?php else : ?>
 					<tr>
-						<td colspan="7" class="text-center">
+						<td colspan="10" class="text-center">
 							Keine Bestellungen vorhanden.
 						</td>
 					</tr>
@@ -114,7 +163,7 @@ $listDirn  = $this->state->get('list.direction');
 			<?php if (!empty($this->items)) : ?>
 				<tfoot>
 					<tr>
-						<td colspan="7">
+						<td colspan="10">
 							<?php echo $this->pagination->getListFooter(); ?>
 						</td>
 					</tr>
